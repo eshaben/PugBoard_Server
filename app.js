1a2b5fe1953deb6e9d95a7449d4d1e93ac01da25
Jonathan Sax <jsax8310@outlook.com>
2017-08-03 11:49:21 -0600

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make sure the following line is above your routes
+app.use(cors());
 
 app.use('/', index);
 app.use('/users', users);
