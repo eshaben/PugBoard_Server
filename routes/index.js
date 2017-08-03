e85d3028c775308fca613460e327e6ac8ea00a92
Jonathan Sax <jsax8310@outlook.com>
2017-08-03 14:25:24 -0600

    added route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('./db/knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('pugboard')
+  .then((data) => {
+    res.json(data)
+  })
 });
 
 module.exports = router;
