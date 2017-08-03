0c4d663f773115e58daa43a84e6b39e6b66e57c7
Jonathan Sax <jsax8310@outlook.com>
2017-08-03 14:39:05 -0600

    debugging

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const knex = require('./db/knex')
+const knex = require('../db/knex')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  knex('pugboard')
+  .then((data) => {
+    res.json(data)
+  })
 });
 
 module.exports = router;
