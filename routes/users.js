c55adb98d92629fdca0124ce54a1dddf61d4caf1
Jonathan Sax <jsax8310@outlook.com>
2017-08-03 12:33:47 -0600

    added more dependencies

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
+var bcrypt = require('bcrypt')
+var jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
